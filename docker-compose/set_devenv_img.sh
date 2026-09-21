566bed31a72351c6fcd0cc4c168c5b281f27d675
fabian.pignataro@freetechsolutions.com.ar <fabian.pignataro@freetechsolutions.com.ar>
2026-09-21 12:38:48 -0300

    img amd rec dataset

--- docker-compose/set_devenv_img.sh
+++ docker-compose/set_devenv_img.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-set -e 
+set -e
 
-sed -i 's/^\(.*\)=docker\.io\/omnileads\/\([^:]*\):.*/\1=\2:latest/' .env
-sed -i 's/^\(.*\)=docker\.io\/freetechsolutions\/\([^:]*\):.*/\1=\2:latest/' .env
+# GNU sed (Linux): sed -i; BSD sed (macOS): sed -i ''
+sed_inplace() {
+  if sed --version >/dev/null 2>&1; then
+    sed -i "$@"
+  else
+    sed -i '' "$@"
+  fi
+}
 
-sed -i "s/ominicontacto.settings.production/ominicontacto.settings.develop/g" .env
-sed -i "s/DJANGO_ENTRYPOINT=init_uwsgi.sh/DJANGO_ENTRYPOINT=init_devenv.sh/g" .env
+sed_inplace 's/^\(.*\)=docker\.io\/omnileads\/\([^:]*\):.*/\1=\2:latest/' .env
+sed_inplace 's/^\(.*\)=docker\.io\/freetechsolutions\/\([^:]*\):.*/\1=\2:latest/' .env
+
+sed_inplace "s/ominicontacto.settings.production/ominicontacto.settings.develop/g" .env
+sed_inplace "s/DJANGO_ENTRYPOINT=init_uwsgi.sh/DJANGO_ENTRYPOINT=init_devenv.sh/g" .env
